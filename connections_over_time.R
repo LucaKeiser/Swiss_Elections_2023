84a2a69faf961bba3d90da5602b148004c0dc5ed
LucaKeiser <luca.keiser@hotmail.com>
2023-10-05 14:57:17 +0200

    update app connections

--- connections_over_time.R
+++ connections_over_time.R
@@ -4,20 +4,21 @@ library(lubridate)
 
 theme_set(theme_minimal())
 
-df_raw <- rsconnect::showMetrics(metricSeries = "container_status",
+df <- rsconnect::showMetrics(metricSeries = "container_status",
                                  metricNames = "connect_count",
                                  appName = "Swiss_Elections_2023",
                                  server = "shinyapps.io",
-                                 from = "2023-09-30")
+                                 from = "2023-09-30") %>% 
+  mutate(connect_count_total = cumsum(connect_count))
 
 
 
-df_raw %>%
-  mutate(connect_count_total = cumsum(connect_count)) %>% 
+df %>%
   ggplot(aes(time, connect_count_total)) + 
-  geom_line(linewidth = 1.5,
+  geom_line(color = "seagreen",
+            linewidth = 2,
             alpha = 0.75) + 
-  geom_vline(xintercept = as_datetime("2023-10-03 11:05:00")) +
+  scale_y_continuous(breaks = seq(0, max(df$connect_count_total), 100)) +
   labs(title =  "Shiny-App-Benutzung",
        x = "Zeit",
        y = "Anzahl Verbindungen (Total)")
